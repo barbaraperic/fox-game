6c5688358ad1fb928ac3b4c5df792f9607633ebd
Barbara Peric <b.peric10@gmail.com>
2021-02-17 16:31:57 +0100

    FEAT: runtime function

---
+++ src/init.js
@@ -0,0 +1,27 @@
+const TICK_RATE = 3000;
+
+
+function tick () {
+  console.log('tick', Date.now());
+}
+
+async function init() {
+  console.log('starting the game')
+
+  let nextTimeToTick = Date.now()
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      tick()
+      nextTimeToTick = now + TICK_RATE
+    }
+
+    requestAnimationFrame(nextAnimationFrame)
+  }
+
+  requestAnimationFrame(nextAnimationFrame)
+}
+
+init()
